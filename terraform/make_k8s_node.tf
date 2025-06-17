25252af5bc194d92c0c49f47ec5e57339584d06b
Neeraj Babu <neerajbabu27@gmail.com>
2025-06-17 23:07:53 +0530

    modify terraform to increase storage for node

--- terraform/make_k8s_node.tf
+++ terraform/make_k8s_node.tf
@@ -33,8 +33,13 @@ resource "aws_instance" "k8s_node_1" {
   provider = aws.ap-south-1
   key_name = "lab"
   vpc_security_group_ids = [aws_security_group.ssh.id]
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+    delete_on_termination = true
+  }
   tags = {
-    Name = "K8s-node_1"
+    Name = "K8s-node-1"
     Terraform = "true"
   }
 }
